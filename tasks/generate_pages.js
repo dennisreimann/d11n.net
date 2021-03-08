1244a8a05e1755aad021756acf0e22763dcd3e80
Dennis Reimann <mail@dennisreimann.de>
2021-03-08 13:30:21 +0100

    🆙 upgrade dependencies

---
+++ tasks/generate_pages.js
@@ -0,0 +1,20 @@
+const pug = require('pug')
+const { mkdirSync, writeFileSync } = require('fs')
+const { dirname, resolve } = require('path')
+const config = require('../pug.config')
+
+const renderPage = (name, data = {}) => {
+  const file = resolve(__dirname, '..', `pages/${name}.pug`)
+  const options = Object.assign({}, config, data)
+  const rendered = pug.renderFile(file, options)
+  const dst = resolve(__dirname, '..', 'dist', `${name}.html`)
+  const dir = dirname(dst)
+
+  mkdirSync(dir, { recursive: true })
+  writeFileSync(dst, rendered)
+}
+
+renderPage('index')
+renderPage('about')
+renderPage('donate')
+renderPage('thankyou')
