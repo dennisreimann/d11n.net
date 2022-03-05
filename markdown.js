c3c54b7a1be15407f53b408a629003563cbfd369
Dennis Reimann <mail@dennisreimann.de>
2022-03-05 21:55:58 +0100

    Upgrade dependencies

--- markdown.js
+++ markdown.js
@@ -2,27 +2,35 @@
 const transformer = require('jstransformer')
 const { _tr: mdTransformer } = transformer(require('jstransformer-markdown-it'))
 const highlightjs = require('highlight.js')
+const anchor = require("markdown-it-anchor");
 
 const config = {
   html: true,
   typographer: true,
-  highlight (code, lang) {
-    const languages = (lang != null) ? [lang] : undefined
-    return highlightjs.highlightAuto(code, languages).value
+  highlight(code, lang) {
+    const languages = lang != null ? [lang] : undefined;
+    return highlightjs.highlightAuto(code, languages).value;
   },
   plugins: [
-    ['markdown-it-anchor', {
-      permalink: true,
-      permalinkBefore: true,
-      permalinkSymbol: '#'
-    }],
-    ['markdown-it-implicit-figures', {
-      figcaption: true,
-      link: true
-    }],
-    ['markdown-it-mark']
-  ]
- }
+    [
+      "markdown-it-anchor",
+      {
+        permalink: anchor.permalink.linkInsideHeader({
+          symbol: '#',
+          placement: "before",
+        }),
+      },
+    ],
+    [
+      "markdown-it-implicit-figures",
+      {
+        figcaption: true,
+        link: true,
+      },
+    ],
+    ["markdown-it-mark"],
+  ],
+};
 
 
 // monkey-patch render function to pass custom options
